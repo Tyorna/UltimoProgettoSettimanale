6574a847fd88b0d13bf7c71bbce0b82924fdc5fd
Francesca Arata <francescaarata@live.it>
2023-08-25 10:32:18 +0200

    progetto vuoto

---
+++ src/main/java/com/example/demo/PSpringSett2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PSpringSett2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PSpringSett2Application.class, args);
+	}
+
+}
